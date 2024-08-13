304f32f63b323356007328e6962c42199076d424
Gigijiwonha <135223202+Gigijiwonha@users.noreply.github.com>
2024-08-13 19:24:46 +1000

    chage images

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-import { useState } from 'react';
+import { useState } from "react";
 import "./App.css";
 import Box from "./component/Box";
 
 const choice = {
   rock: {
     name: "rock",
-    img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.smithsonianmag.com%2Fsmart-news%2Feverything-everywhere-all-at-onces-racaccoonie-hot-dog-fingers-and-more-for-sale-at-auction-180981691%2F&psig=AOvVaw0bJnzAp2gxpOVoFJ9ssXG_&ust=1723617080539000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLD6ppCs8YcDFQAAAAAdAAAAABAR",
+    img: "https://nationaltoday.com/wp-content/uploads/2021/08/National-Pet-Rock-Day-1200x834.jpg",
   },
   paper: {
     name: "paper",
-    img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fagrandelife.net%2Fchewbacca-paper-bag-puppet%2F&psig=AOvVaw0Avwg-P07L01UiZK3aAXIS&ust=1723620090296000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKDgq6238YcDFQAAAAAdAAAAABAE",
+    img: "https://as1.ftcdn.net/v2/jpg/05/61/91/44/1000_F_561914463_0d0fbbIm8Qu1t24Cz01ZNvNh1g3NSLB0.jpg",
   },
   scissors: {
     name: "scissors",
-    img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fclipartmag.com%2Ftag%2Fscissors&psig=AOvVaw0Wbte1R-e8r3CHbwnDfPEk&ust=1723620110923000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIiLr7a38YcDFQAAAAAdAAAAABAR",
+    img: "https://t3.ftcdn.net/jpg/00/55/10/40/360_F_55104038_VMb5Bfm1cCVyh93VbJIoK4AQrlmyH0Wc.jpg",
   },
 };
 
 function App() {
+  const [userSelect, setUserSelect] = useState(null);
 
   const play = (userChoice) => {
-    console.log("play>>>>", userChoice);
-    if (userChoice == "rock") {
-      console.log("it's rock!")
-    }
+    // console.log("play>>>>", userChoice);
+    // if (userChoice == "rock") {
+    //   console.log("it's rock!")
+    // }
+    setUserSelect(choice[userChoice]);
   };
 
   return (
     <div>
-      <div className='main'>
-        <Box name="User" />
-        <Box name="Computer" />
+      <div className="main">
+        <Box name="User" item={userSelect} />
+        {/* <Box name="Computer" /> */}
       </div>
-      <div className='main'>
-        <button onClick={()=> play("rock")}>Rock</button>
-        <button onClick={()=>play("paper")}>Paper</button>
-        <button onClick={()=>play("scissors")}>Scissors</button>
+      <div className="main">
+        <button onClick={() => play("rock")}>Rock</button>
+        <button onClick={() => play("paper")}>Paper</button>
+        <button onClick={() => play("scissors")}>Scissors</button>
       </div>
     </div>
   );
