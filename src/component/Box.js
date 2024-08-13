7a7420fb4031e433161a93b15959b3d2055cbb40
Gigijiwonha <135223202+Gigijiwonha@users.noreply.github.com>
2024-08-13 19:37:40 +1000

    fix error

--- src/component/Box.js
+++ src/component/Box.js
@@ -4,7 +4,7 @@ function Box(props) {
   return (
     <div className="box">
       <h1 className="name">{props.name}</h1>
-      <img className="images" src={props.item && props.item.img, alt={props.item ? props.item.name : ''}}></img>
+      <img className="images" src={props.item && props.item.img}></img>
       <h2 className="result">WIN</h2>
     </div>
   );
